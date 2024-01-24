b8f483e27d8019e9b306a7e1564d94dea15af45d
PrajwalVK7 <prajwalvk98@gmail.com>
2024-01-24 10:57:00 +0530

    cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,45 +1,55 @@
 import React from 'react'
 import { Col, Row, Table } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { removeFromCart } from '../redux/cartSlice';
+import { removeFromCart,addToCart, decrementCount, incrementCount } from '../redux/cartSlice';
 
 function Cart() {
   const dispatch = useDispatch()
   const cartArray = useSelector((state) => state.cartReducer);
-  // console.log("cartArray",cartArray.price)
   let totalPrice = 0;
   if (cartArray.length > 0) {
     for (let i = 0; i < cartArray.length; i++) {
-      totalPrice += cartArray[i].price
+      totalPrice += cartArray[i].price*cartArray[i].count
     }
   }
-  console.log(totalPrice)
+  // console.log(totalPrice)
   return (
     <>
       <h2 className='text-center'>Cart</h2>
-      <div className='container mt-5 mb-5'>
+      <div className='container-fluid mt-5 mb-5'>
         <Row className='mt-5 mb-5'>
           <Col lg={6}>
             <Table className='mb-5'>
-              {/* <thead>
+              <thead>
                 <tr>
-                  <th>#</th>
-                  <th>Product</th>
-                  <th>image</th>
-                  <th>Price</th>
+                  <th>#Items</th>
+                  <th></th>
+                  <th>Qty</th>
+                  <th>Subtotal</th>
                 </tr>
-              </thead> */}
+              </thead>
               {cartArray?.length > 0 ?
                 cartArray.map((item, index) => (
                   <tbody>
                     <tr key={item.id} style={{ fontSize: '20px' }}>
-                      <td>{index + 1}</td>
-                      <td>{item.title}</td>
+                      {/* <td>{index + 1}</td> */}
                       <td><img src={item.thumbnail} alt={item.title} height={"150px"} width={"100%"} /></td>
-                      <td className='fw-bolder text-success d-flex align-items-center justify-content-center' style={{ fontSize: '25px' }}><h3>{item.price}</h3></td>
-                      <td><i class="fa-solid fa-trash" onClick={() => dispatch(removeFromCart(item.id))}></i></td>
+                      <td>{item.title}
+                        <h3>Rs: {item.price}</h3>
+                        <h6>(Inclusive of all tax)</h6>
+                        <i class="fa-solid fa-trash" onClick={() => dispatch(removeFromCart(item.id))}></i>
+                      </td>
+                      <td >
+                        <div className='card rounded d-inline-block w-100 d-flex justify-content-center align-items-center '>
+                        <div className='d-flex justify-content-evenly p-2 align-items-center '> 
+                        <button className='btn'><i class="fa-solid fa-minus" onClick={()=>dispatch(decrementCount(item.id))}></i></button>
+                        <span className='shade me-5 ms-5'>{item.count}</span>
+                        <button className='btn'><i class="fa-solid fa-plus" onClick={()=>dispatch(incrementCount(item.id))}></i></button>
+                        </div>
+                      </div></td>
+                      <td>Rs : <span>{item.price}</span></td>
                     </tr>
-                  </tbody>
+                  </tbody> 
                 )) : <p>no data</p>
               }
             </Table>
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,19 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-
 const cartSlice = createSlice({
-        name:"cart",
-        initialState:[],
-        reducers:{
-            addToCart:(state,action)=>{
-                state.push(action.payload)
-            },
-            removeFromCart:(state,action)=>{
-                return state.filter(item=>item.id!==action.payload);
+    name: "cart",
+    initialState: [],
+    reducers: {
+        addToCart: (state, action) => {
+            const existingItem = state.find(item => item.id === action.payload.id);
+            if (existingItem) {
+                return state.map(item => (
+                    item.id === action.payload.id ? { ...item, count: item.count + 1 } : item
+                ));
+            } else {
+                return [...state, { ...action.payload, count: 1 }];
             }
+        },
+        removeFromCart: (state, action) => {
+            return state.filter(item => item.id !== action.payload);
+        },
+        incrementCount: (state, action) => {
+            const itemId = action.payload;
+            const newState = state.map(item => (
+                item.id === itemId ? { ...item, count: item.count + 1 } : item
+            ));
+            return newState;
+        },
+        decrementCount: (state, action) => {
+            const itemId = action.payload;
+            const newState = state.map(item => (
+                item.id === itemId && item.count > 1 ? { ...item, count: item.count - 1 } : item
+            ));
+            return newState;
         }
-})
+    }
+});
 
 export default cartSlice.reducer;
-export const {addToCart,removeFromCart} = cartSlice.actions;
+export const { addToCart, removeFromCart, incrementCount, decrementCount } = cartSlice.actions;
